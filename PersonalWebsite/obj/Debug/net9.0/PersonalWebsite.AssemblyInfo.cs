e5627a4098cb52732922d52fe91827c6c21ea121
KrabbyPattyson <originalbrycekrispies@gmail.com>
2025-01-10 20:23:10 -0500

    More initial files? 1000+ is bonkers but at least it runs.

--- PersonalWebsite/obj/Debug/net9.0/PersonalWebsite.AssemblyInfo.cs
+++ PersonalWebsite/obj/Debug/net9.0/PersonalWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3edf0d357cc8c052e148127a6893b10d6d4b0dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821881cc51e3676ed3745659f624a56771eba646")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
